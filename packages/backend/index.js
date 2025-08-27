9a9fb43dc7adf95244cfb794b0cf298b40e3a5e3
Fausto Oliva <fausto.calavera@gmail.com>
2025-08-27 00:45:26 -0300

    Middleware de logueo

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -10,8 +10,21 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+// Middleware para loguear hora y endpoint
+app.use((req, res, next) => {
+  const now = new Date().toISOString();
+  console.log(`[${now}] ${req.method} ${req.originalUrl}`);
+  console.log(req.body ?? {}); // Log del body de la solicitud
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use
 
 app.use("/users", UserRouter);
 app.use("/appointments", AppointmentRouter);
